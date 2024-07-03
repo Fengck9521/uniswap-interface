f07a8498a07492d27467e95a31be38c287561bd1
StareMe <2207597682@qq.com>
2024-07-03 10:11:27 +0800

    uniswapv2-interface

---
+++ src/constants/multicall/index.ts
@@ -0,0 +1,13 @@
+import { ChainId } from 'graycykswap-sdk'
+import MULTICALL_ABI from './abi.json'
+
+const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
+  [ChainId.MAINNET]: '0xeefBa1e63905eF1D7ACbA5a8513c70307C1cE441',
+  [ChainId.ROPSTEN]: '0x53C43764255c17BD724F74c4eF150724AC50a3ed',
+  [ChainId.KOVAN]: '0x2cc8688C5f75E365aaEEb4ea8D6a480405A48D2A',
+  [ChainId.RINKEBY]: '0x42Ad527de7d4e9d9d011aC45B31D8551f8Fe9821',
+  [ChainId.GÖRLI]: '0x77dCa2C955b15e9dE4dbBCf1246B4B85b651e50e',
+  [ChainId.SEPOLIA]: '0xaDB4B398C7c9a6951777882a638d6fe076ae983c'
+}
+
+export { MULTICALL_ABI, MULTICALL_NETWORKS }
